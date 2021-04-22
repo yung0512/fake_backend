f1207eddee229fbb15f86beae4af582877849f41
yung0512 <jeff8651215@gmail.com>
2021-04-22 18:35:28 +0800

    backend

---
+++ api/user/routers/user.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controller/userController");
+const auth = require("../../../config/auth");
+router.post("/register", userController.registerNewUser);
+router.post("/login", userController.loginUser);
+router.get("/me", auth, userController.getUserDetails);
+module.exports = router;
